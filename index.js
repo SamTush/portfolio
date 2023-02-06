9a705de0e371671960be9b05a5d869b899a2c188
SamTush <samo4421@icloud.com>
2023-02-06 12:55:54 +0300

    Add js, link it and add onclick listener

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("humburger").onclick = function() {myFunction()};
+
+function myFunction() {
+  document.getElementById("show").style.display = "block";
+  console.log("Clicked");
+}
