d92bf9bbc69fb29206baec63f505eed7ebd38c2f
SamTush <samo4421@icloud.com>
2023-02-06 15:12:08 +0300

    Fix stylelint and EsLint errors

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-document.getElementById("humburger").onclick = function() {myFunction()};
-document.getElementById("close-icon").onclick = function() {closeIcon()};
-document.getElementById("click-a").onclick = function() {closeIcon()};
-document.getElementById("click-b").onclick = function() {closeIcon()};
-document.getElementById("click-c").onclick = function() {closeIcon()};
- 
 function myFunction() {
-  document.getElementById("show").style.display = "block";
+  document.getElementById('show').style.display = "block";
   console.log("Clicked");
-}
-
+};
 function closeIcon() {
-  document.getElementById("show").style.display = "none";
-}
-
+  document.getElementById('show').style.display = "none";
+};
+document.getElementById('humburger').onclick = function() { myFunction() };
+document.getElementById('close-icon').onclick = function() { closeIcon() };
+document.getElementById('click-a').onclick = function() { closeIcon() };
+document.getElementById('click-b').onclick = function() { closeIcon() };
+document.getElementById('click-c').onclick = function() { closeIcon() };
