5b26aecb27b90819662b11a1c4aaa2563aa990e0
SamTush <samo4421@icloud.com>
2023-02-06 17:46:07 +0300

    Solve ESLint errors finally

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-
-document.getElementById('humburger').addEventListener('click', myFunction);
-document.getElementById('close-icon').addEventListener('click', closeIcon);
-document.getElementById('click-a').addEventListener('click', closeIcon);
-document.getElementById('click-b').addEventListener('click', closeIcon);
-document.getElementById('click-c').addEventListener('click', closeIcon);
 function myFunction() {
   document.getElementById('show').style.display = 'block';
 }
 function closeIcon() {
   document.getElementById('show').style.display = 'none';
 }
+document.getElementById('humburger').addEventListener('click', myFunction);
+document.getElementById('close-icon').addEventListener('click', closeIcon);
+document.getElementById('click-a').addEventListener('click', closeIcon);
+document.getElementById('click-b').addEventListener('click', closeIcon);
+document.getElementById('click-c').addEventListener('click', closeIcon);
