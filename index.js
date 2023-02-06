ff8b18db05e6d6aa3dd0984aa26f72b8fdc0ca4b
SamTush <samo4421@icloud.com>
2023-02-06 17:44:03 +0300

    Change js to addEventListener to solve ESLint errors

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+
+document.getElementById('humburger').addEventListener('click', myFunction);
+document.getElementById('close-icon').addEventListener('click', closeIcon);
+document.getElementById('click-a').addEventListener('click', closeIcon);
+document.getElementById('click-b').addEventListener('click', closeIcon);
+document.getElementById('click-c').addEventListener('click', closeIcon);
 function myFunction() {
   document.getElementById('show').style.display = 'block';
 }
 function closeIcon() {
   document.getElementById('show').style.display = 'none';
 }
-document.getElementById('humburger').onclick = function () { myFunction() };
-document.getElementById('close-icon').onclick = function () { closeIcon() };
-document.getElementById('click-a').onclick = function () { closeIcon() };
-document.getElementById('click-b').onclick = function () { closeIcon() };
-document.getElementById('click-c').onclick = function () { closeIcon() };
