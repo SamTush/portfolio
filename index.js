7fcec9a3e6215f4f79f25bb37d3af9215a42f22f
SamTush <samo4421@icloud.com>
2023-02-06 14:54:51 +0300

    Add menu pop up

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 document.getElementById("humburger").onclick = function() {myFunction()};
-
+document.getElementById("close-icon").onclick = function() {closeIcon()};
+document.getElementById("click-a").onclick = function() {closeIcon()};
+document.getElementById("click-b").onclick = function() {closeIcon()};
+document.getElementById("click-c").onclick = function() {closeIcon()};
+ 
 function myFunction() {
   document.getElementById("show").style.display = "block";
   console.log("Clicked");
 }
+
+function closeIcon() {
+  document.getElementById("show").style.display = "none";
+}
+
