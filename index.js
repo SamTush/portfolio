869224ac663740c1f9c9399cfbb6aa0b00574a37
SamTush <samo4421@icloud.com>
2023-02-06 15:16:52 +0300

    Fix ESLint errors

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 function myFunction() {
-  document.getElementById('show').style.display = "block";
-  console.log("Clicked");
-};
+  document.getElementById('show').style.display = 'block';
+}
 function closeIcon() {
-  document.getElementById('show').style.display = "none";
-};
-document.getElementById('humburger').onclick = function() { myFunction() };
-document.getElementById('close-icon').onclick = function() { closeIcon() };
-document.getElementById('click-a').onclick = function() { closeIcon() };
-document.getElementById('click-b').onclick = function() { closeIcon() };
-document.getElementById('click-c').onclick = function() { closeIcon() };
+  document.getElementById('show').style.display = 'none';
+}
+document.getElementById('humburger').onclick = function () { myFunction() };
+document.getElementById('close-icon').onclick = function () { closeIcon() };
+document.getElementById('click-a').onclick = function () { closeIcon() };
+document.getElementById('click-b').onclick = function () { closeIcon() };
+document.getElementById('click-c').onclick = function () { closeIcon() };
