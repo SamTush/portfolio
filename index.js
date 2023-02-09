39b01c7706cf2ee77cb787b14aafa49341440243
SamTush <samo4421@icloud.com>
2023-02-09 11:53:33 +0300

    Remove ESLint and Stylelint error part Three

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // General variables
 const linkLive = 'https://samtush.github.io/Profolio-setup-and-mibile-first/';
 const linkSource = 'https://github.com/SamTush';
-// construstor for pop-up content 
+// construstor for pop-up content
 class Content {
   constructor(title, image, buttonLive, buttonSource) {
     this.title = title;
@@ -33,7 +33,7 @@ const content4 = new Content(
   'images/Snapshoot Portfolio4.png',
   linkLive,
   linkSource,
-); 
+);
 // variables to set appearance
 const sectionCard = document.querySelector('#portfolio');
 const cardSection = document.createElement('div');
@@ -178,7 +178,7 @@ function closeCard() {
 }
 document.getElementById('humburger').addEventListener('click', myFunction);
 // for loops
-for (let i = 0; i < (document.querySelectorAll('.click')).length; i++) {
+for (let i = 0; i < (document.querySelectorAll('.click')).length; i+=1) {
   document.querySelectorAll('.click')[i].addEventListener('click', closeIcon);
 }
 // button array
@@ -200,58 +200,58 @@ buttonArray.forEach((buttonItem, index) => {
   buttonItem.addEventListener('click', () => {
     const popupCard = document.createElement('div');
     sectionCard.appendChild(popupCard);
-  // inner html for pop-up
-  popupCard.innerHTML = `
-  <!--popup mobile-->
-  <div class="target-mobile">
-    <div id="popup-card-menu" class="popup-card" data-target="p-1">
-        <div class="card-popup">
-            <div class="popup-header">
-                <h2>${item.title}</h2>
-                <img id="closing" class="closePopup" src="images/IconB.png" alt="closing">
-            </div>
-            <div class="canopy">
-                <h4>CANOPY</h4>
-                <ul class="back">
-                    <li><img class="dot" src="images/Counter.png" alt="dot">Back End Dev</li>
-                    <li><img class="dot" src="images/Counter.png" alt="dot">2015</li>
-                </ul>
-            </div>
-            <div class="popup-body">
-                <img src="${item.image}" alt="Snapshoot-Portfolio">
-                <div class="popup-body-desktop">
-                <div class="p-div-desktop">
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-                        the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
-                        of type and scrambled it to make a type specimen book.
-                    </p>
+    // inner html for pop-up
+    popupCard.innerHTML = `
+      <!--popup mobile-->
+      <div class="target-mobile">
+        <div id="popup-card-menu" class="popup-card" data-target="p-1">
+            <div class="card-popup">
+                <div class="popup-header">
+                    <h2>${item.title}</h2>
+                    <img id="closing" class="closePopup" src="images/IconB.png" alt="closing">
                 </div>
-                <div class="side">
-                    <ul class="small-btn-div">
-                        <li class="small-btn"><a>html</a></li>
-                        <li class="small-btn"><a>css</a></li>
-                        <li class="small-btn"><a>javaScript</a></li>
+                <div class="canopy">
+                    <h4>CANOPY</h4>
+                    <ul class="back">
+                        <li><img class="dot" src="images/Counter.png" alt="dot">Back End Dev</li>
+                        <li><img class="dot" src="images/Counter.png" alt="dot">2015</li>
                     </ul>
-                    <ul class="small-btn-div">
-                        <li class="small-btn"><a>github</a></li>
-                        <li class="small-btn"><a>ruby</a></li>
-                        <li class="small-btn"><a>Bootstrap</a></li>
-                    </ul>
-                    <hr>
-                    <div class="popup-button">
-                        <button class="icon2"><a href="${item.buttonLive}">See live</a></button>
-                        <button class="icon3"><a href="${item.buttonSource}">See Source</a></button>
+                </div>
+                <div class="popup-body">
+                    <img src="${item.image}" alt="Snapshoot-Portfolio">
+                    <div class="popup-body-desktop">
+                    <div class="p-div-desktop">
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                            the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                            of type and scrambled it to make a type specimen book.
+                        </p>
+                    </div>
+                    <div class="side">
+                        <ul class="small-btn-div">
+                            <li class="small-btn"><a>html</a></li>
+                            <li class="small-btn"><a>css</a></li>
+                            <li class="small-btn"><a>javaScript</a></li>
+                        </ul>
+                        <ul class="small-btn-div">
+                            <li class="small-btn"><a>github</a></li>
+                            <li class="small-btn"><a>ruby</a></li>
+                            <li class="small-btn"><a>Bootstrap</a></li>
+                        </ul>
+                        <hr>
+                        <div class="popup-button">
+                            <button class="icon2"><a href="${item.buttonLive}">See live</a></button>
+                            <button class="icon3"><a href="${item.buttonSource}">See Source</a></button>
+                        </div>
                     </div>
                 </div>
-            </div>
+                </div>
             </div>
         </div>
-    </div>
-  </div>
-  `;
-  document.querySelector('.target-mobile').style.display = 'block';
-  const cloCard = document.querySelector('.closePopup');
-  cloCard.addEventListener('click', closeCard);
+      </div>
+    `;
+    document.querySelector('.target-mobile').style.display = 'block';
+    const cloCard = document.querySelector('.closePopup');
+    cloCard.addEventListener('click', closeCard);
   });
 });
